2774ff0f5a6e18667d68d9084bf5878e21dbd0ae
DuandSu <61254078+DuandSu@users.noreply.github.com>
2020-02-27 13:01:53 -0700

    Comp 100C - Basic Syntax 27-Feb

---
+++ 01-getting-started/src/scripts/comp100CFunctions.js
@@ -0,0 +1,34 @@
+// define attributes / variables
+//     - number
+//     - string
+//     - boolean
+//     - array
+//     - dictionary / objects
+//     - undefined
+// sample if / else
+// arrays
+//     - add to the front
+//     - add to the end
+//     - update values
+// loops 
+//     - for
+//     - for/in
+//     - while
+//     - do while
+//     - forEach (with array and function)
+// Objects / Dictionaries
+//     - declare object
+//     - lookup key to retrieve value
+// functions
+//     - parameters
+//     - returns
+
+const comp100CFunctions = {
+
+    add3: (num1, num2, num3) => {
+        //return num1 + num2 + num3;
+    }
+
+};
+
+export default comp100CFunctions;
