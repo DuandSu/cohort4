5ba0b75a8c4f9ba3464f408e8beda6b5fb0a38c8
DuandSu <61254078+DuandSu@users.noreply.github.com>
2020-03-06 15:34:20 -0700

    C100D - Working with Array section done 6-Mar

--- 01-getting-started/src/scripts/main.js
+++ 01-getting-started/src/scripts/main.js
@@ -1,6 +1,8 @@
 import functions from './functions.js';
 import calculator from './calculator.js';
 import taxCalculator from './taxcalculator.js';
+import ArraysDictionaries from './arraysdictionaries.js';
+
 
 // **********
 //
@@ -10,10 +12,10 @@ import taxCalculator from './taxcalculator.js';
 idNumber.addEventListener('change', (() => {
     idNumberSize.textContent = functions.size(idNumber.value);
 }));
+
 //
-// Note that used ".textContent" and ".innerHTML" to play and test. Seems they do the same
-// thing visually anyways. Normally would stick with one, but left as is for future reference.
-//
+// Event listeners for Calculator
+// 
 addBtn.addEventListener("click", (() => {
     document.getElementById("calculateAction").textContent = calculator.addTwoNumbers(
         Number(document.getElementById("inputField1").value), 
@@ -21,24 +23,56 @@ addBtn.addEventListener("click", (() => {
 }));
 
 subtractBtn.addEventListener("click", (() => {
-    document.getElementById("calculateAction").innerHTML = calculator.subtractTwoNumbers(
+    document.getElementById("calculateAction").textContent = calculator.subtractTwoNumbers(
         Number(document.getElementById("inputField1").value), 
         Number(document.getElementById("inputField2").value));
 }));
 
 multiplyBtn.addEventListener("click", (() => {
-    document.getElementById("calculateAction").innerHTML = calculator.multiplyTwoNumbers(
+    document.getElementById("calculateAction").textContent = calculator.multiplyTwoNumbers(
         Number(document.getElementById("inputField1").value), 
         Number(document.getElementById("inputField2").value));
 }));
 
 divideBtn.addEventListener("click", (() => {
-    document.getElementById("calculateAction").innerHTML = calculator.divideTwoNumbers(
+    document.getElementById("calculateAction").textContent = calculator.divideTwoNumbers(
         Number(document.getElementById("inputField1").value), 
         Number(document.getElementById("inputField2").value));
 }));
-
+//
+// Event listeners for Tax Calculator
+// 
 calculateTaxBtn.addEventListener("click", (() => {
-    document.getElementById("calculateTax").innerHTML = taxCalculator.calculateFederalTax(
+    document.getElementById("calculateTax").textContent = taxCalculator.calculateFederalTax(
         Number(document.getElementById("inputGrossIncome").value));
+}));
+//
+// Event listeners and JS for Working with Arrays
+// 
+// Define array used for Competency 100C
+//
+
+let competency100Array = [];
+
+addArrayBtn.addEventListener("click", (() => {
+    if (isNaN(document.getElementById("inputArrayValue").value))
+        document.getElementById("arrayMessage").textContent = "Input is not a valid number.";
+    else {
+        competency100Array = ArraysDictionaries.addToArray(competency100Array, 
+            Number(document.getElementById("inputArrayValue").value));
+        document.getElementById("arrayMessage").textContent = "Number was added to the array."
+    }
+}));
+
+showArrayBtn.addEventListener("click", (() => {
+    document.getElementById("arrayMessage").textContent = ArraysDictionaries.displayArray(competency100Array)
+}));
+
+totalArrayBtn.addEventListener("click", (() => {
+    document.getElementById("arrayMessage").textContent = ArraysDictionaries.totalArray(competency100Array)
+}));
+
+clearArrayBtn.addEventListener("click", (() => {
+    competency100Array = ArraysDictionaries.clearArray(competency100Array);
+    document.getElementById("arrayMessage").textContent = "All array entries cleared. Array is now empty"
 }));
