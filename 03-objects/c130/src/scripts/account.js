ff082a54db35ea582d10237495c7f2059ad63e18
DuandSu <duane.w.munro@gmail.com>
2020-03-27 15:39:45 -0600

    Reorganized folders closer to Instructions

---
+++ 03-objects/c130/src/scripts/account.js
@@ -0,0 +1,14 @@
+// This is competency 130a.
+// This is the Account Class definition.
+//
+class Account {
+
+    constructor(acctName, acctBal) {
+
+        this.acctName = acctName;
+        this.acctBal = acctBal;
+    
+    }
+}
+
+export default Account;
