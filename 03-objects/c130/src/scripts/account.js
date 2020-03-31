ddb0e8cfb68e0bbbcc6553afe45da03d4ffede1b
DuandSu <duane.w.munro@gmail.com>
2020-03-31 00:03:55 -0600

    C130b: 31-Mar-2020: Completed AccountController

--- 03-objects/c130/src/scripts/account.js
+++ 03-objects/c130/src/scripts/account.js
@@ -5,7 +5,23 @@ export class Account {
     constructor(acctName, acctBal) {
 
         this.acctName = acctName;
+        this.acctNum = 0;
         this.acctBal = acctBal;
+        this.acctType = "Normal";
+        
+    }
+    
+    setToCredit() {
+
+        this.acctType = "Credit";
+        return true;
+    
+    }
+    
+    isCredit() {
+
+        if (this.acctType === "Credit") return true;
+        else return false;
     
     }
 
@@ -36,11 +52,96 @@ export class Account {
 
 export class AccountController {
 
-    constructor(acctName, acctBal) {
+    constructor(clientName) {
 
-        this.acctName = acctName;
-        this.acctBal = acctBal;
+        this.clientName = clientName;
+        this.listOfAccts = [];
+        this.nextAcctNum = 1;
     
     }
+
+    getClientName() {
+
+        return this.clientName;
+
+    }
+
+    addAccount(acctObj) {
+
+        acctObj.acctNum = this.nextAcctNum++;
+        this.listOfAccts[acctObj.acctNum] = acctObj;
+            
+        return acctObj.acctNum;
+
+    }
+
+    getAcctName(acctNum) {
+
+        return this.listOfAccts[acctNum].getAccountName();
+
+    }
+
+    getAcctBalance(acctNum) {
+
+        // console.log(`Get Account Name for Account Number: ${acctNum}`);
+        // console.log(`Client Name: ${this.clientName}`);
+        // console.log(`Account Number for Account ${acctNum}: ${this.listOfAccts[acctNum].acctNum}`);
+        // console.log(`Account Name for Account ${acctNum}: ${this.listOfAccts[acctNum].acctName}`);
+        // console.log(`Account Type for Account ${acctNum}: ${this.listOfAccts[acctNum].acctType}`);
+        // console.log(`Account Balance for Account ${acctNum}: ${this.listOfAccts[acctNum].acctBal}`);
+        // console.log(`Next Account Number: ${this.nextAcctNum}`);
+
+        return this.listOfAccts[acctNum].getBalance();
+
+    }
+
+    removeAccount(acctNum) {
+
+        const emptyAccount = new Account();
+        return this.listOfAccts[acctNum] = emptyAccount;
+            
+    }
+    
+    sumAccounts() {
+
+        let sumAccts = 0;
+        for (let i = 1; i < this.listOfAccts.length; i++) {
+            if (typeof this.listOfAccts[i].getAccountName() !== 'undefined') {
+                sumAccts += this.listOfAccts[i].getBalance();
+            }
+        }
+    return sumAccts;
+    }
+
+    findHighAccount() {
+
+        if (this.listOfAccts.length > 0) {
+            let highestAcctNum = 1;
+            let highestAcctBal = this.listOfAccts[1].getBalance();
+            for (let i = 1; i < this.listOfAccts.length; i++) {
+                if (typeof this.listOfAccts[i].getAccountName() !== 'undefined') {
+                    if (this.listOfAccts[i].getBalance() > highestAcctBal)
+                        highestAcctNum = i;
+                }
+            }
+            return highestAcctNum;
+        }
+        return 0;
+    }
+
+    findLowAccount() {
+
+        if (this.listOfAccts.length > 0) {
+            let lowestAcctNum = 1;
+            let lowestAcctBal = this.listOfAccts[1].getBalance();
+            for (let i = 1; i < this.listOfAccts.length; i++) {
+                if (typeof this.listOfAccts[i].getAccountName() !== 'undefined') {
+                    if (this.listOfAccts[i].getBalance() < lowestAcctBal)
+                        lowestAcctNum = i;
+                }
+            }
+            return lowestAcctNum;
+        }
+        return 0;
+    }
 }
-// export default Account;
--- 03-objects/c130/src/scripts/account.test.js
+++ 03-objects/c130/src/scripts/account.test.js
@@ -1,12 +1,13 @@
 import {Account, AccountController} from './account.js'
 
-test('130a: Does class instantiation and methods work?', () => {
+test('130a/b: Does class instantiation and methods work?', () => {
 
     const savingsAccount = new Account("Savings", 100);
     expect(savingsAccount.acctName).toBe("Savings");
     expect(savingsAccount.getAccountName()).toBe("Savings");
     expect(savingsAccount.acctBal).toBe(100);
     expect(savingsAccount.getBalance()).toBe(100);
+    expect(savingsAccount.isCredit()).toBeFalsy();;
     expect(savingsAccount.deposit(150)).toBe(250);
     expect(savingsAccount.getBalance()).toBe(250);
     expect(savingsAccount.withdraw(50)).toBe(200);
@@ -15,11 +16,74 @@ test('130a: Does class instantiation and methods work?', () => {
     const checkingAccount = new Account("Chequing", 25);
     expect(checkingAccount.acctName).toBe("Chequing");
     expect(checkingAccount.getAccountName()).toBe("Chequing");
-    expect(checkingAccount.acctBal).toBe(25);
     expect(checkingAccount.getBalance()).toBe(25);
+    expect(checkingAccount.isCredit()).toBeFalsy();;
     expect(checkingAccount.deposit(10)).toBe(35);
     expect(checkingAccount.getBalance()).toBe(35);
     expect(checkingAccount.withdraw(30)).toBe(5);
     expect(checkingAccount.getBalance()).toBe(5);
+    
+    const creditCardAccount = new Account("MasterCard", 0);
+    expect(creditCardAccount.getAccountName()).toBe("MasterCard");
+    expect(creditCardAccount.getBalance()).toBe(0);
+    expect(creditCardAccount.setToCredit()).toBeTruthy();
+    expect(creditCardAccount.isCredit()).toBeTruthy();
+    
+    const lineOfCreditAccount = new Account("Line of Credit", -200);
+    expect(lineOfCreditAccount.getAccountName()).toBe("Line of Credit");
+    expect(lineOfCreditAccount.getBalance()).toBe(-200);
+    expect(lineOfCreditAccount.setToCredit()).toBeTruthy();
+    expect(lineOfCreditAccount.isCredit()).toBeTruthy();;
+
+    //
+    // Create AccountController for client Duane Munro
+    //
+    const duane = new AccountController("Duane Munro");
+    expect(duane.getClientName()).toBe("Duane Munro");
+    //
+    // Add the prior Savings Account
+    //
+    expect(duane.addAccount(savingsAccount)).toBe(1);
+    expect(duane.getAcctName(1)).toBe("Savings");
+    expect(duane.getAcctBalance(1)).toBe(200);
+    expect(duane.listOfAccts[1].isCredit()).toBeFalsy();
+    //
+    // Add the prior Chequing Account
+    //
+    expect(duane.addAccount(checkingAccount)).toBe(2);
+    expect(duane.getAcctName(2)).toBe("Chequing");
+    expect(duane.getAcctBalance(2)).toBe(5);
+    expect(duane.listOfAccts[2].isCredit()).toBeFalsy();
+    //
+    // Add the prior Credit Card Account
+    //
+    expect(duane.addAccount(creditCardAccount)).toBe(3);
+    expect(duane.getAcctName(3)).toBe("MasterCard");
+    expect(duane.getAcctBalance(3)).toBe(0);
+    expect(duane.listOfAccts[3].isCredit()).toBeTruthy();
+    //
+    // Add the prior Line of Credit Account
+    //
+    expect(duane.addAccount(lineOfCreditAccount)).toBe(4);
+    expect(duane.getAcctName(4)).toBe("Line of Credit");
+    expect(duane.getAcctBalance(4)).toBe(-200);
+    expect(duane.listOfAccts[4].isCredit()).toBeTruthy();
+    //
+    // Remove the MasterCard Credit Account
+    //
+    const emptyAccount = new Account();
+    expect(duane.removeAccount(3)).toEqual(emptyAccount);
+    //
+    // Summarize the total of all Accounts    //
+    //
+    expect(duane.sumAccounts()).toBe(5);
+    //
+    // Find Highest Value Account    //
+    //
+    expect(duane.findHighAccount()).toBe(1);
+    //
+    // Find Lowest Value Account    //
+    //
+    expect(duane.findLowAccount()).toBe(4);
 
 });
