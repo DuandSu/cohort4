62952ac73fdce0a62ca5368cd2ba037e5b0508a0
DuandSu <duane.w.munro@gmail.com>
2020-04-17 13:55:26 -0600

    C920: 17-Apr-2020: Refactor Udemy Tests

--- 03-objects/src/scripts/c920.test.js
+++ 03-objects/src/scripts/c920.test.js
@@ -1,16 +1,23 @@
 //
 // Jest requires the following to allow fetch to work in Test Files: npm install --save isomorphic-fetch es6-promise
 //
-// Note that console.logs will work but display message, "Cannot log after tests are done. Did you forget to wait for 
-// something async in your test?". This includes the log for the test comparison. Result is that the test may actually
-// pass, but the delayed log will show the error. So important to page up and view regardless.
+// Note that it is important to include "async (done)" after the comma in the test function declaration. Then include "await"
+// before each async function that is called. This will cause the test to wait for completion of the async function. The 
+// purpose of the "done();" statement at the end is so that the results of the test can be compiled before reporting the 
+// final test results. Larry said this might be optional and do by default now, but definitely the "async" 
+// before each async function is required.
 //
-// Ensure you import below.
+// Otherwise the console.logs will work but display message, "Cannot log after tests are done. Did you forget to wait for 
+// something async in your test?". This includes the log for the test comparison. Result is that the test results will
+// indicate a pass, even though there was an error, and the delayed log will show the error. You can still page up and view 
+// but better to include the "async done" and "await".
+//
+// Ensure you import "isomorphic-fetch" below.
 //
 
 import "isomorphic-fetch";
 
-test('920: Udemy Fetch Assignments', () => {
+test('920: Discovery for Udemy Fetch Assignments', () => {
     //
     // numbers is an array of numbers
     //
@@ -50,19 +57,32 @@ test('920: Udemy Fetch Assignments', () => {
     expect(urls).toEqual(['https://jsonplaceholder.typicode.com/users',
     'https://jsonplaceholder.typicode.com/posts',
     'https://jsonplaceholder.typicode.com/albums']);
+});
 
+test('920: Udemy Fetch Assignment #1', async (done) => {
+    
     async function fetchUsers() {
         try {
-        const resp = await fetch('https://jsonplaceholder.typicode.com/users')
-        const data = await resp.json();
-        expect(data.length).toBe(10);
-        return data;
+            const resp = await fetch('https://jsonplaceholder.typicode.com/users')
+            const data = await resp.json();
+            expect(data.length).toBe(10);
+            return data;
         } catch (err) {
             console.log("Ooops", err);
         }
     }
-    
-    fetchUsers();
+
+    await fetchUsers();
+    done();
+});
+
+test('920: Udemy Fetch Assignment #2 as taught with .next()', async (done) => {
+
+    const urls = [
+        'https://jsonplaceholder.typicode.com/users',
+        'https://jsonplaceholder.typicode.com/posts',
+        'https://jsonplaceholder.typicode.com/albums'
+        ]
     
     const getData = async function() {
         try {
@@ -77,8 +97,18 @@ test('920: Udemy Fetch Assignments', () => {
         }
     }
 
-    getData();
-    
+    await getData();
+    done();
+});
+
+test('920: Udemy Fetch Assignment #2 & #3 with my answer to replace .next() with async await', async (done) => {
+
+    const urls = [
+        'https://jsonplaceholder.typicode.com/users',
+        'https://jsonplaceholder.typicode.com/posts',
+        'https://jsonplaceholder.typicode.com/albums'
+        ]
+
     const getDataDWM = async function() {
         try {
             const [ users, posts, albums ] = await Promise.all(urls.map(async function (url) {
@@ -94,7 +124,7 @@ test('920: Udemy Fetch Assignments', () => {
         }
     }
 
-    getDataDWM();
+    await getDataDWM();
 
     
     const urlsX = [
@@ -115,13 +145,15 @@ test('920: Udemy Fetch Assignments', () => {
                 expect(posts.length).toBe(100);
                 expect(albums.length).toBe(100);
             } catch (err) {
-                expect(err).toStrictlyEqual(`Beer`);
-                // expect(err).toStrictlyEqual(`FetchError`);
+                // expect(err).toStrictlyEqual(`Beer`);
+                // expect(err).toContain(`FetchError`);
                 // expect(err).toStrictlyEqual(`[FetchError: request to https://jsonplaceholderTYPO.typicode.com/albums failed, reason: getaddrinfo ENOTFOUND jsonplaceholdertypo.typicode.com]`);
             }
         }
         
-        getDataDWMX();
+        // await getDataDWMX();
+
+        done();
 
         // expect(getDataDWMX).toThrow();
     });
