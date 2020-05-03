8d97c6eabca429bae1fe1cf7e2f4151399753784
DuandSu <duane.w.munro@gmail.com>
2020-05-03 13:36:56 -0600

    C130D 3-May-2020: Completed play with Try Catch and scope

---
+++ 03-objects/src/scripts/c130d.test.js
@@ -0,0 +1,181 @@
+import community from './community.js';
+import c130d from './c130d.js'
+
+global.fetch = require('node-fetch'); // Was Larry's solution vs the isomorphic-fetch solution that I found.
+
+import c920 from './fetch.js'
+
+// import "isomorphic-fetch";
+
+let consoleLog = [];
+let consoleLine = 0;
+
+const url = 'http://localhost:5000/';
+const urlBad = 'http://localhost:4000/';
+// const urlBad = 'https://192.168.0.69';
+
+test('130d: Testing the TDD Pipes', () => {
+    
+    consoleLog = [];
+    consoleLine = 0;
+    consoleLog[consoleLine++] = "Testing the TDD pipes";
+    expect(consoleLog[0]).toBe("Testing the TDD pipes");
+    
+});
+
+test('130d: Play Area with Try Catch Block and scope references', () => {
+    
+    let testScope = function (xParam) {
+
+        consoleLog[consoleLine++] = "Global Entered testScope Function = " + xGlobal;
+        consoleLog[consoleLine++] = "Param Entered testScope Function = " + xParam;
+        let xFunction = "xFunction";
+        consoleLog[consoleLine++] = "Function Entered testScope Function = " + xFunction;
+        try {
+            let xTry = "xTry";
+            consoleLog[consoleLine++] = "Global in Try = " + xGlobal;
+            consoleLog[consoleLine++] = "Param in Try = " + xParam;
+            consoleLog[consoleLine++] = "Function in Try = " + xFunction;
+            consoleLog[consoleLine++] = "Try in Try = " + xTry;
+        }
+        catch {
+            let xCatch = "xCatch";
+            consoleLog[consoleLine++] = "Global in Catch = " + xGlobal;
+            consoleLog[consoleLine++] = "Param in Catch = " + xParam;
+            consoleLog[consoleLine++] = "Function in Catch = " + xFunction;
+            consoleLog[consoleLine++] = "Try in Catch = " + xTry;
+            consoleLog[consoleLine++] = "Catch in Catch = " + xCatch;
+        }
+        consoleLog[consoleLine++] = "Global Before Return = " + xGlobal;
+        consoleLog[consoleLine++] = "Param Before Return = " + xParam;
+        consoleLog[consoleLine++] = "Function Before Return = " + xFunction;
+        // consoleLog[consoleLine++] = "Try Before Return = " + xTry; // ReferenceError: xTry is not defined. Not caught because after try - catch block.
+        // consoleLog[consoleLine++] = "Catch Before Return = " + xCatch;
+        return "xReturn";
+    }
+    
+    consoleLog = [];
+    consoleLine = 0;
+    
+    let xGlobal = "xGlobal";
+    consoleLog[consoleLine++] = "Global Declaration = " + xGlobal;
+    
+    let xReturn = testScope("xParam");
+    
+    consoleLog[consoleLine++] = "Global After Exit testScope Function = " + xGlobal;
+    consoleLog[consoleLine++] = "Return After Exit testScope Function = " + xReturn;
+    
+    expect(consoleLog[0]).toBe("Global Declaration = xGlobal");
+    expect(consoleLog[1]).toBe("Global Entered testScope Function = xGlobal");
+    expect(consoleLog[2]).toBe("Param Entered testScope Function = xParam");
+    expect(consoleLog[3]).toBe("Function Entered testScope Function = xFunction");
+    expect(consoleLog[4]).toBe("Global in Try = xGlobal");
+    expect(consoleLog[5]).toBe("Param in Try = xParam");
+    expect(consoleLog[6]).toBe("Function in Try = xFunction");
+    expect(consoleLog[7]).toBe("Try in Try = xTry");
+    expect(consoleLog[8]).toBe("Global Before Return = xGlobal");
+    expect(consoleLog[9]).toBe("Param Before Return = xParam");
+    expect(consoleLog[10]).toBe("Function Before Return = xFunction");
+    // expect(consoleLog[8]).toBe("Try Before Return = xTry"); // ReferenceError: xTry is not defined. Not caught because after try - catch block.
+    expect(consoleLog[11]).toBe("Global After Exit testScope Function = xGlobal");
+    expect(consoleLog[12]).toBe("Return After Exit testScope Function = xReturn");
+    
+    testScope = function (xParam) {
+        
+        consoleLog[consoleLine++] = "Global Entered testScope Function = " + xGlobal;
+        consoleLog[consoleLine++] = "Param Entered testScope Function = " + xParam;
+        let xFunction = "xFunction";
+        consoleLog[consoleLine++] = "Function Entered testScope Function = " + xFunction;
+        try {
+            let xTry = "xTry";
+            consoleLog[consoleLine++] = "Global in Try = " + xGlobal;
+            consoleLog[consoleLine++] = "Param in Try = " + xParam;
+            consoleLog[consoleLine++] = "Function in Try = " + xFunction;
+            consoleLog[consoleLine++] = "Try in Try = " + xTry;
+            
+            // Trigger reference error to catch. Skips over code to end of try block
+            // or simply jumps to catch block.
+            
+            console.log(yNotDefined);
+            
+            consoleLog[consoleLine++] = "Global Skipped after Error = " + xGlobal;
+            consoleLog[consoleLine++] = "Param Skipped after Error = " + xParam;
+            consoleLog[consoleLine++] = "Try Skipped after Error = " + xTry;
+        }
+        catch {
+            let xCatch = "xCatch";
+            consoleLog[consoleLine++] = "Global in Catch = " + xGlobal;
+            consoleLog[consoleLine++] = "Param in Catch = " + xParam;
+            consoleLog[consoleLine++] = "Function in Catch = " + xFunction;
+            // consoleLog[consoleLine++] = "Try in Catch = " + xTry; // ReferenceError: xTry is not defined. Not caught because after try block.
+            consoleLog[consoleLine++] = "Catch in Catch = " + xCatch;
+        }
+        consoleLog[consoleLine++] = "Global Before Return = " + xGlobal;
+        consoleLog[consoleLine++] = "Param Before Return = " + xParam;
+        consoleLog[consoleLine++] = "Function Before Return = " + xFunction;
+        // consoleLog[consoleLine++] = "Try Before Return = " + xTry; // ReferenceError: xTry is not defined. Not caught because after try block.
+        // consoleLog[consoleLine++] = "Catch Before Return = " + xCatch;
+        return "xReturn";
+    }
+    
+    consoleLog = [];
+    consoleLine = 0;
+    
+    consoleLog[consoleLine++] = "Global Declaration = " + xGlobal;
+    
+    xReturn = testScope("xParam");
+    
+    consoleLog[consoleLine++] = "Global After Exit testScope Function = " + xGlobal;
+    consoleLog[consoleLine++] = "Return After Exit testScope Function = " + xReturn;
+    
+    expect(consoleLog[0]).toBe("Global Declaration = xGlobal");
+    expect(consoleLog[1]).toBe("Global Entered testScope Function = xGlobal");
+    expect(consoleLog[2]).toBe("Param Entered testScope Function = xParam");
+    expect(consoleLog[3]).toBe("Function Entered testScope Function = xFunction");
+    expect(consoleLog[4]).toBe("Global in Try = xGlobal");
+    expect(consoleLog[5]).toBe("Param in Try = xParam");
+    expect(consoleLog[6]).toBe("Function in Try = xFunction");
+    expect(consoleLog[7]).toBe("Try in Try = xTry");
+    expect(consoleLog[8]).toBe("Global in Catch = xGlobal");
+    expect(consoleLog[9]).toBe("Param in Catch = xParam");
+    expect(consoleLog[10]).toBe("Function in Catch = xFunction");
+    // expect(consoleLog[8]).toBe("Try in Catch = xTry"); // ReferenceError: xTry is not defined. Not caught because after try block.
+    expect(consoleLog[11]).toBe("Catch in Catch = xCatch");
+    expect(consoleLog[12]).toBe("Global Before Return = xGlobal");
+    expect(consoleLog[13]).toBe("Param Before Return = xParam");
+    expect(consoleLog[14]).toBe("Function Before Return = xFunction");
+    // expect(consoleLog[8]).toBe("Try Before Return = xTry"); // ReferenceError: xTry is not defined. Not caught because after try block.
+    expect(consoleLog[15]).toBe("Global After Exit testScope Function = xGlobal");
+    expect(consoleLog[16]).toBe("Return After Exit testScope Function = xReturn");
+    
+});
+
+test('130d: Async ASP Interface with Community', async (done) => {
+
+    
+    // let data = await c130d.confirmAPIConnect (urlBad);
+    // expect(data.status).not.toBe(200);
+    // expect(data.status).toContain(`Error`);
+    
+    // data = await c920.postData(url + "clear");
+    // expect(data.status).toBe(200);
+
+    // data = await c130d.confirmAPIConnect (url);
+    // expect(data.status).toBe(200);
+    
+    // const canada = new community.Community ("Canada");
+    // data = await c920.postData(url + 'add', canada.cityList[0]);
+    // expect(data.status).toEqual(200);
+
+    // data = await c920.postData(url + 'all');
+    // expect(data.status).toEqual(200);
+
+    // expect(data.length).toBe(1); 
+    // expect(data[0].key).toBe(0);
+    // expect(data[0].nextKey).toBe(1);
+
+    // data = await c130d.confirmAPIConnect (url);
+    // expect(data.status).toBe(200);
+
+    done();
+});
--- 03-objects/src/scripts/fetch.js
+++ 03-objects/src/scripts/fetch.js
@@ -95,32 +95,37 @@ const c920 = {
 
     async postData(url = '', data = {}) {
         // Default options are marked with *
-        const response = await fetch(url, {
-            method: 'POST',     // *GET, POST, PUT, DELETE, etc.
-            mode: 'cors',       // no-cors, *cors, same-origin
-            cache: 'no-cache',  // *default, no-cache, reload, force-cache, only-if-cached
-            credentials: 'same-origin', // include, *same-origin, omit
-            headers: {
-                'Content-Type': 'application/json'
-                // 'Content-Type': 'application/x-www-form-urlencoded',
-            },
-            redirect: 'follow',         // manual, *follow, error
-            referrer: 'no-referrer',    // no-referrer, *client
-            body: JSON.stringify(data)  // body data type must match "Content-Type" header
-        });
-        // if (response.ok) {
-            // console.log("HTTP-Status: " + response.status);
-            // return await response.json();   // parses JSON response into native JavaScript objects
-        // }
-        // else {
-            // console.log("HTTP-Error: " + response.status);
-        // }
+        try {
 
-        const json = await response.json();    // parses JSON response into native JavaScript objects
-        json.status = response.status;
-        json.statusText = response.statusText;
-    
-        return json;
+            const response = await fetch(url, {
+                method: 'POST',     // *GET, POST, PUT, DELETE, etc.
+                mode: 'cors',       // no-cors, *cors, same-origin
+                cache: 'no-cache',  // *default, no-cache, reload, force-cache, only-if-cached
+                credentials: 'same-origin', // include, *same-origin, omit
+                headers: {
+                    'Content-Type': 'application/json'
+                    // 'Content-Type': 'application/x-www-form-urlencoded',
+                },
+                redirect: 'follow',         // manual, *follow, error
+                referrer: 'no-referrer',    // no-referrer, *client
+                body: JSON.stringify(data)  // body data type must match "Content-Type" header
+            });
+                    
+            const json = await response.json();    // parses JSON response into native JavaScript objects
+            json.status = response.status;
+            json.statusText = response.statusText;
+
+            return json;
+
+        } catch (err) {
+
+            const catchErr = {};
+            catchErr.status = err.name;
+            catchErr.statusText = err.message;
+
+            return catchErr;
+
+        }
     }
 };
 
