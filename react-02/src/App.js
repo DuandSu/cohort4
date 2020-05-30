47706bc88c743920c01b8801651a6291a49fbed1
DuandSu <duane.w.munro@gmail.com>
2020-05-30 01:06:55 -0600

    C140A 30-May-2020: react-02: Done Adding SVG.

---
+++ react-02/src/App.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import logo from './svg/logo.svg';
+import shipST from './svg/star-trek-svgrepo-com.svg';
+import insigniaST from './svg/section_31_geeksvgs.com.svg';
+import insigniaKL1 from './svg/star_trek_klingon_insignia__geeksvgs.com.svg';
+import insigniaKL2 from './svg/star_trek_33_geeksvgs.com.svg';
+import kittyKL from './svg/klingon_kitty_geeksvgs.com.svg';
+import spockHI from './svg/star_trek_27_geeksvgs.com.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+        <div className="App-svg-area">
+            <div>
+              <img src={shipST} className="App-svg" alt="shipST" />
+            </div>
+            <div>
+              <img src={insigniaST} className="App-svg" alt="insigniaST" />
+            </div>
+            <div>
+              <img src={insigniaKL1} className="App-svg" alt="insigniaKL1" />
+            </div>
+            <div>
+              <img src={insigniaKL2} className="App-svg" alt="insigniaKL2" />
+            </div>
+            <div>
+              <img src={kittyKL} className="App-svg" alt="kittyKL" />
+            </div>
+            <div>
+              <img src={spockHI} className="App-svg" alt="spockHI" />
+            </div>
+        </div>
+        <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <p>
+                Edit <code>src/App.js</code> and save to reload.
+              </p>
+              <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learn React
+              </a>
+            </header>
+        </div>
+    </div>
+  );
+}
+
+export default App;
