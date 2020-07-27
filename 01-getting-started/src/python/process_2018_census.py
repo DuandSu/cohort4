80a417b5566cddb444576c7fbb91a658b171a0e9
DuandSu <duane.w.munro@gmail.com>
2020-07-26 23:08:36 -0600

    C220 26-Jul-2020: Completed

--- 01-getting-started/src/python/process_2018_census.py
+++ 01-getting-started/src/python/process_2018_census.py
@@ -1,12 +1,22 @@
 import census
+#
+# Note to come back later and refactor to automatically load in the dictionary "censusDefn". It might
+# actually require an if statement to avoid the MULTIPOLYGON issue. No "Ifs" for competency.
+#
+# Note that am already essentially loading "censusRecord" automatically. Just define at beginning
+# same as "censusResCntByCS". No need to pre-define in census.py.
+#
+# Also note that definitely used pytest to test necessary functionality. Never felt need though to
+# break out into functions for easier test scripting though.
+#
+censusResCntByCS = {}
 
-print ("-------------------------------------------------------------------------------")
 f = open("Census_by_Community_2018.csv", "r")
 
 # Read past the titles row
 f.readline()
 countLines = 1
-
+# Read every read until EOF
 for x in f:
     
     x += ","
@@ -17,22 +27,37 @@ for x in f:
     startChar = 0
     endChar = 0
     multiPolygon = x.index("MULTIPOLYGON")
-
+    #
+    # Parse through the record poplating the dictionary record.
+    # Note that to meet special no "IF" conditions I am simply ignoring the last column which
+    # is the MULTIPOLYGON, which I don't need to complete this competency. This column is surrounded 
+    # by brackets, but has commas inside. To properly parse this strange beast I would need to add 
+    # extra logic that requires an IF. Therefore, treating it almost like and EOL.
+    #
     while startChar <= recordLen and startChar < multiPolygon:
         commaChar = x.index(",", startChar)
         value = x[startChar: commaChar]
-        print(f"startChar = {startChar}; commaChar = {commaChar}; value = {value}")
         census.censusRecord[currColumn] = value
         currColumn += 1
         startChar = commaChar + 1
-
     currClass = census.censusRecord[census.censusDefn["CLASS"]]
     currSector = census.censusRecord[census.censusDefn["SECTOR"]]
     currResCnt = census.censusRecord[census.censusDefn["RES_CNT"]]
-    print (f'CLASS: {currClass}; SECTOR: {currSector}; RES_CNT: {currResCnt}')
 
-print(f'\n-- Calgary 2018 Census by Community had {countLines} lines.')
+    try:
+        censusResCntByCS[(currClass, currSector)] += int(currResCnt)
+    except:
+        censusResCntByCS[(currClass, currSector)] = int(currResCnt)
 
 f.close()
-print("")
-print("-------------------------------------------------------------------------------")
+
+f = open("report.txt", "w")
+f.write("-------------------------------------------------------------------------------")
+f.write(f'-- Calgary 2018 Census by Community had {countLines} lines.')
+f.write("-------------------------------------------------------------------------------")
+
+for resByCS in censusResCntByCS:
+    f.write(f'---- Class/Sector {resByCS[0]}/{resByCS[1]} has ResCnt Total: {"{:,.0f}".format(censusResCntByCS[(resByCS[0],resByCS[1])])}')
+f.write("-------------------------------------------------------------------------------")
+
+f.close()
