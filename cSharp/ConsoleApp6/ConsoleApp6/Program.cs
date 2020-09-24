1f0c5b4236245d4e88cb6bcc9536b6bf4b0c2338
DuandSu <duane.w.munro@gmail.com>
2020-09-24 13:43:57 -0600

    24-Sep-2020: C# Tutorial: Completed Branches & Loops

--- cSharp/ConsoleApp6/ConsoleApp6/Program.cs
+++ cSharp/ConsoleApp6/ConsoleApp6/Program.cs
@@ -69,6 +69,40 @@ namespace ConsoleApp6
                 Console.WriteLine($"Hello World! The counter is {counter}");
                 counter++;
             } while (counter < 10);
+
+            for (int counter1 = 0; counter1 < 10; counter1++)
+            {
+                Console.WriteLine($"Hello World! The counter1 is {counter1}");
+            }
+
+            for (int row = 1; row < 11; row++)
+            {
+                Console.WriteLine($"The row is {row}");
+            }
+
+            for (char column = 'a'; column < 'k'; column++)
+            {
+                Console.WriteLine($"The column is {column}");
+            }
+
+            for (int row = 1; row < 11; row++)
+            {
+                for (char column = 'a'; column < 'k'; column++)
+                {
+                    Console.WriteLine($"The cell is ({row}, {column})");
+                }
+            }
+            Console.WriteLine("Sum of all integers 1 through 20 that are divisible by 3:");
+            short sumDiv3 = 0;
+            for (short i = 1; i <= 20; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine($"Adding: {i}");
+                    sumDiv3 += i;
+                }
+            }
+            Console.WriteLine($"Answer is {sumDiv3}");
         }
     }
 }
