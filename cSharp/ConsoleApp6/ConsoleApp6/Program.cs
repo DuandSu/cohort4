54d8ce66d069ea96da83896a5646fd5aa026c66b
DuandSu <duane.w.munro@gmail.com>
2020-09-24 15:47:49 -0600

    24-Sep-2020: C# Tutorial: Completed C# Interactive Tutorial

--- cSharp/ConsoleApp6/ConsoleApp6/Program.cs
+++ cSharp/ConsoleApp6/ConsoleApp6/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ConsoleApp6
 {
@@ -92,7 +93,7 @@ namespace ConsoleApp6
                     Console.WriteLine($"The cell is ({row}, {column})");
                 }
             }
-            Console.WriteLine("Sum of all integers 1 through 20 that are divisible by 3:");
+            Console.WriteLine("Challenge: Sum of all integers 1 through 20 that are divisible by 3:");
             short sumDiv3 = 0;
             for (short i = 1; i <= 20; i++)
             {
@@ -103,6 +104,66 @@ namespace ConsoleApp6
                 }
             }
             Console.WriteLine($"Answer is {sumDiv3}");
+
+            var names = new List<string> { "Duane", "Ana", "Felipe" };
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+            }
+
+            Console.WriteLine("---------------------------");
+            names.Add("Maria");
+            names.Add("Bill");
+            names.Add("Bill"); 
+            names.Remove("Ana");
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+            }
+
+            Console.WriteLine($"My name is {names[0]}.");
+            Console.WriteLine($"I've added {names[2]} and {names[3]} to the list.");
+            Console.WriteLine($"The list has {names.Count} people in it");
+            var index = names.IndexOf("Felipe");
+            if (index != -1)
+                Console.WriteLine($"The name {names[index]} is at index {index}");
+
+            var notFound = names.IndexOf("Not Found");
+            Console.WriteLine($"When an item is not found, IndexOf returns {notFound}");
+
+            names.Sort();
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+            }
+
+            var fibonacciNumbers = new List<int> { 1, 1 };
+
+            var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+            var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+
+            fibonacciNumbers.Add(previous + previous2);
+
+            foreach (var item in fibonacciNumbers)
+                Console.WriteLine(item);
+
+            Console.WriteLine("Challenge: Fibonacci Numbers:Generate the first 20 numbers in the sequence:");
+
+            fibonacciNumbers.Clear();
+            fibonacciNumbers.Add(1);
+            int prevNumber = 0;
+            int currNumber = 1;
+            int newNumber;
+
+            for (int i = 1; i < 20; i++)
+            {
+                newNumber = prevNumber + currNumber;
+                fibonacciNumbers.Add(newNumber);
+                prevNumber = currNumber;
+                currNumber = newNumber;
+            }
+            foreach (var item in fibonacciNumbers)
+                Console.WriteLine(item);
         }
     }
 }
